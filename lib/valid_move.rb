7a803eef13bc0876a109563c886a13973081059e
Maria  Morris <mariasemailbox@gmail.com>
2018-05-02 23:05:19 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,26 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board_array, location)
+  if board_array[location] == " " || board_array[location] == "" || board_array[location] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def location_exists?(number)
+  if number.between? (0,8) == true
+    return true
+  else
+    return false
+  end
+end
+
+if (position_taken?(board_array, location)) == false && (location_exists?(number) == true)
+  return true
+else
+  return false
+  end
+end
